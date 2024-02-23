8a3e926924fc1ef6cb2ca5dc2ccba60b1aa31a4f
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-02-23 15:10:51 -0500

    Crea la configuración inicial de webpack como plantilla

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+import './reset.css' // reset de estilos
+import './styles.css' // estilos del proyecto
+
+console.log('carajo')
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/lib/index.js',
+
+    // devtool: 'inline-source-map',
+    devServer: {
+        static: './public',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            // filename: '[name].css',
+        }),
+    ],
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+        clean: true,
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    // MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'style-loader',
+                        options: { injectType: 'singletonStyleTag'},
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff2|woff|ttf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [new CssMinimizerPlugin(), '...'],
+    },
+};
