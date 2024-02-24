1b21265024703f79cadffe865d4713b051102d18
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-02-24 12:24:42 -0500

    Incorpora babel y sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,60 +2,63 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { argv } = require('process');
 
-module.exports = (env) => {
-    const [mode, modeConf] =
-        env.mode === 'development'
-            ? ['development', true]
-            : ['production', false];
+const [mode, modeConf] = argv.find((e) => e === 'serve')
+    ? ['development', true]
+    : ['production', false];
 
-    return {
-        mode,
-        entry: './src/lib/index.js',
+module.exports = {
+    mode,
+    entry: './src/lib/index.js',
 
-        devtool: modeConf ? 'inline-source-map' : false,
-        devServer: {
-            static: './public',
-        },
+    devtool: modeConf ? 'inline-source-map' : false,
+    devServer: {
+        static: './public',
+    },
 
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: 'src/index.html',
-                inject: 'body',
-            }),
-            new MiniCssExtractPlugin({
-                filename: '[name].css',
-            }),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+        clean: !modeConf,
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(s?css)$/i,
+                use: [
+                    modeConf ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff2|woff|ttf)$/i,
+                type: 'asset/resource',
+            },
         ],
-
-        output: {
-            filename: 'main.js',
-            path: path.resolve(__dirname, 'public'),
-            clean: !modeConf,
-            assetModuleFilename: 'assets/[name][ext]',
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: [
-                        modeConf ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        'css-loader',
-                    ],
-                },
-                {
-                    test: /\.(jpg|jpeg|gif|png)$/i,
-                    type: 'asset/resource',
-                },
-                {
-                    test: /\.(woff2|woff|ttf)$/i,
-                    type: 'asset/resource',
-                },
-            ],
-        },
-        optimization: {
-            minimizer: [new CssMinimizerPlugin(), '...'],
-        },
-    };
+    },
+    optimization: {
+        minimizer: [new CssMinimizerPlugin(), '...'],
+    },
 };
